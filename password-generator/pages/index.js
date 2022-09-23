ebe3cb15b1311ad4b60066255d59ce5eaf875a22
Matt Brannon <mattbrannon.dev@gmail.com>
2022-09-23 09:28:50 -0500

    add reducer function

--- password-generator/pages/index.js
+++ password-generator/pages/index.js
@@ -1,23 +1,52 @@
 import styled from 'styled-components';
 
-export default function PasswordGenerator() {
-  return (
-    <Mobile>
-      <Container>
-        <Title>Password Generator</Title>
-        <Main>
-          <Top>
-            <Columns>
-              <Password>password</Password>
-              <Span>copy button</Span>
-            </Columns>
-          </Top>
-          <Bottom></Bottom>
-        </Main>
-      </Container>
-    </Mobile>
-  );
-}
+import { calculateStrength, generatePassword, getSettingsCount } from '../api';
+import { Bottom, Boxes, Main } from '../components/Layouts';
+
+import Button from '../components/Button';
+import Checkbox from '../components/Checkbox';
+import GeneratedPassword from '../components/GeneratedPassword';
+import Meter from '../components/Meter';
+import RangeSlider from '../components/RangeSlider';
+
+const preferences = {
+  hidePassword: false,
+  showEntropy: false,
+};
+
+const initialState = {
+  uppercase: false,
+  lowercase: false,
+  numbers: false,
+  symbols: false,
+  length: 8,
+  preferences,
+};
+
+const uppercase = 'uppercase';
+const lowercase = 'lowercase';
+const numbers = 'numbers';
+const symbols = 'symbols';
+const length = 'length';
+const hidden = 'hidePassword';
+const entropy = 'showEntropy';
+
+const reducer = (state, action) => {
+  switch (action.name) {
+    case uppercase:
+    case lowercase:
+    case numbers:
+    case symbols:
+    case length: {
+      return { ...state, [action.name]: action.value };
+    }
+    case hidden:
+    case entropy: {
+      const preferences = { ...state.preferences, [action.name]: action.value };
+      return { ...state, preferences };
+    }
+  }
+};
 
 const Mobile = styled.div`
   padding: 64px 16px;
