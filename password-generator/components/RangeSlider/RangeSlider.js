d3c9f8f392b6b0a1311ee899784151d302fad48d
Matt Brannon <mattbrannon.dev@gmail.com>
2022-09-23 09:02:48 -0500

    add Range Slider component

---
+++ password-generator/components/RangeSlider/RangeSlider.js
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+import { useState } from 'react';
+import { Wrapper, Top, Input, ValueLabel, LengthValue } from './styles';
+
+export const RangeSlider = ({ state: { length }, ...props }) => {
+  const [value, setValue] = useState(length);
+  const stepValue = Number((100 / props.max) * value);
+
+  const onChange = (e) => {
+    props.dispatch({ name: props.name, value: e.target.value });
+    setValue(e.target.value);
+  };
+
+  return (
+    <Wrapper>
+      <Top>
+        <ValueLabel>Character Length</ValueLabel>
+        <LengthValue>{value}</LengthValue>
+      </Top>
+      <Input
+        onChange={onChange}
+        value={value}
+        stepValue={stepValue}
+        {...props}
+      />
+    </Wrapper>
+  );
+};
---
+++ password-generator/components/RangeSlider/styles.js
@@ -0,0 +1,79 @@
+import styled, { css } from 'styled-components';
+
+const calculateGradient = (stepValue) => {
+  const gradient = `linear-gradient(
+    90deg, 
+    var(--green) 0%, 
+    var(--green) ${Math.min(stepValue, 99)}%,
+    var(--veryDarkGrey) ${stepValue}%,
+    var(--veryDarkGrey) 99%
+  )`;
+
+  return gradient;
+};
+
+export const Input = styled.input.attrs(({ stepValue }) => {
+  return {
+    type: 'range',
+    style: {
+      '--gradient': calculateGradient(stepValue),
+    },
+  };
+})`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background: var(--gradient);
+  width: 100%;
+  height: 8px;
+
+  &::-webkit-slider-thumb {
+    ${() => thumb}
+  }
+
+  ::-moz-range-thumb {
+    ${() => thumb}
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  gap: 16px;
+  padding: 16px 0;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ValueLabel = styled.span`
+  display: flex;
+  flex-wrap: wrap;
+  color: var(--white);
+  font-size: var(--regular);
+`;
+
+export const LengthValue = styled.div`
+  color: var(--green);
+  font-size: var(--large);
+`;
+
+const thumb = css`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  height: 28px;
+  width: 28px;
+  border-radius: 50%;
+
+  background: var(--white);
+  &:hover {
+    background: var(--green);
+  }
+
+  &:active {
+    background: var(--veryDarkGrey);
+    border: 2px solid var(--green);
+  }
+`;
